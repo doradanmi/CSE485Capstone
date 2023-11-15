d88253da44b37b0ec5ff429179b6805f2dc1378c
Nhu Huynh <87301653+doradanmi@users.noreply.github.com>
2023-11-15 00:10:56 -0700

    Added GetFlights

--- Mapping/src/TimeSlider.jsx
+++ Mapping/src/TimeSlider.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
 export default function TimeSlider({ time, setTime }) {
-  return (
-    <div className="flex items-center">
-      <input
-        type="range"
-        min="0"
-        max="86400"
-        value={time}
-        onChange={(e) => setTime(e.target.value)}
-        className="w-full"
-      />
-      <div className="ml-4">{time}</div>
-    </div>
-  );
+	let date = new Date(null);
+	date.setSeconds(time); // specify value of time here
+	let timeString = date.toISOString().substr(11, 8);
+	return (
+		<div className="flex items-center">
+			<input
+				type="range"
+				min="0"
+				max="86400"
+				value={time}
+				onChange={(e) => setTime(e.target.value)}
+				className="w-full"
+			/>
+			<div className="ml-4">{timeString}</div>{" "}
+		</div>
+	);
 }
